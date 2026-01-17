299a794d320a60cd2f8af86b34d1352d2137a0ad
Veer-git-hub <veeragrawal907@gmail.com>
2026-01-17 18:16:54 +0530

    Add files via upload
    these are the python for my first assignment

---
+++ Veer2.py
@@ -0,0 +1,3 @@
+first_name=input("Enter Your first Name: ")
+last_name=input("Enter Your last Name: ")
+print(f"Hello {first_name} {last_name}! We are Humble with Your Presence")
---
+++ veer.py
@@ -0,0 +1,7 @@
+A=int(input("Enter The First Number: "))
+B=int(input("Enter The Second Number: "))
+
+print("Addition:", A+B)
+print("Subtraction:", A-B)
+print("Multiplication:", A*B)
+print("Division:", A/B)
